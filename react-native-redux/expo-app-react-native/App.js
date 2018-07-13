daf45d1ede57102245239f82ddbef977e6ed7858
Vasundhara N <vnalkund@arubanetworks.com>
2018-07-13 11:21:21 +0530

    push react-native-redux

---
+++ react-native-redux/expo-app-react-native/App.js
@@ -0,0 +1,24 @@
+
+import React, { Component } from 'react';
+import {
+ AppRegistry,
+ StyleSheet,
+ Text,
+ View
+} from 'react-native';
+export default class App extends React.Component {
+  render() {
+    console.log("herll");
+    return (
+      <View style={styles.container}>
+        
+        
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+ container: {
+ flex: 1,
+ },
+});
